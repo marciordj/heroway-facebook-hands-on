23a8391e6561f9dcf69ce24db61734dbbf5ca20c
Gtosta96 <gabrieltosta3@gmail.com>
2019-12-12 21:17:42 -0300

    fix facebook logo

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import { ReactComponent as FacebookLogo } from '../../../assets/img/facebook-logo.svg';
+import FacebookLogo from '../../../assets/img/facebook-logo.svg';
 
 const Header: React.FC = () => {
   return (
     <header className="header">
       <div className="logo">
-        <FacebookLogo />
+        <img src={FacebookLogo} alt="Facebook Logo" />
       </div>
     </header>
   );
